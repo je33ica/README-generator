1d4967bd2eba204048f5f3b87e9e679311de8471
je33ica <je331687@gmail.com>
2021-01-07 12:21:21 +0000

    started working on Qs & checking using console log

--- index.js
+++ index.js
@@ -1,6 +1,100 @@
 const inquirer = require("inquirer");
 const fs = require ('fs');
 
+// array of questions for user
+inquirer
+  .prompt([
+    // {
+    //     type: "input",
+    //     name: "Author",
+    //     message: "What is your name?"
+    // },
+    // {
+    //     type: "input",
+    //     name: "Github",
+    //     message: "What is your Github username?"
+    // },
+    // {
+    //     type: "input",
+    //     name: "title",
+    //     message: "What is the name of your project?"
+    // },
+    // {
+    //     type: "input",
+    //     name: "Description",
+    //     message: "Please, provide a description of your project?"
+    // },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Directions for using this project?',
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+      },
+    {
+      type: "input",
+      name: "instDep",
+      message: "Command to install dependencies?",
+      default: "npm install",
+    },
+    {
+      type: "input",
+      name: "useage",
+      message: "Please enter anything the user needs to know for this repo?",
+    },
+    {
+      type: "input",
+      name: "contribute",
+      message:
+        "Please enter any information the user needs to know about contributing.",
+    },
+    {
+      type: "checkbox",
+      message: "What languages have you used?",
+      name: "language used",
+      choices: [
+        {
+          name: "Javascript",
+        },
+        {
+          name: "Python",
+        },
+        {
+          name: "HTML",
+        },
+        {
+          name: "Java",
+        },
+        {
+          name: "C#",
+        },
+        {
+          name: "C",
+        },
+        {
+          name: "C++",
+        },
+        {
+          name: "CSS",
+        },
+      ],
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What license does your droject require?",
+      choices: ["MIT", "Apache", "GPL", "IBM", "ISC", "Mozilla", "Unlicense"],
+    },
+  ])
+  .then((answer) => {
+    console.log("Your project is called " + answer.title);
+    console.log("Project description " + answer.description);
+    console.log("licesne " + answer.license);
+  });
+  
 // // function to write README file
 // function writeToFile(fileName, data) {
 // }
