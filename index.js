7b51d4a7b8edbabb0e1fd4bdb7b5a50b0d0c839f
je33ica <je331687@gmail.com>
2021-01-07 12:17:28 +0000

    added psuedo code for inquirer Qs

---
+++ index.js
@@ -0,0 +1,48 @@
+const inquirer = require("inquirer");
+const fs = require ('fs');
+
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
+
+// Name
+// Choose a self-explaining name for your project.
+
+// Description
+// Let people know what your project can do specifically.
+//  Provide context and add a link to any reference visitors might be unfamiliar with.
+//  A list of Features or a Background subsection can also be added here.
+// If there are alternatives to your project, this is a good place to list differentiating factors.
+
+// Badges
+
+// Installation
+// Listing specific steps helps remove ambiguity and gets people to using your project as quickly as possible.
+//  If it only runs in a specific context like a particular programming language version
+//  or operating system or has dependencies that have to be installed manually, also add a Requirements subsection.
+
+//  Usage
+// Use examples liberally, and show the expected output if you can.
+// It's helpful to have inline the smallest example of usage that you can demonstrate,
+//  while providing links to more sophisticated examples if they are too long to reasonably include in the README.
+
+// Support
+// Tell people where they can go to for help. It can be any combination of an issue tracker, a chat room, an email address, etc.
+// //  explain and showcase what your application does,
+// //  justify the technologies used
+// //   challenges you faced
+// //   features you hope to implement in the future
+
+// //  how to install
+// authors & acknolegement
+// how to use
+// how to colloborate
+// license
