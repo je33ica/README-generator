a0218a5bfa607ef922f5a10e904d4aee46cf114d
je33ica <je331687@gmail.com>
2021-01-10 15:11:59 +0000

    changed layout of index & generate radme so they correspond properly

--- generateMD.js
+++ generateMD.js
@@ -1,36 +1,59 @@
 // function to generate markdown for README
 function generateMD(data) {
-    return `# ${data.title}
+  return `# ${data.title}
+    # This README has been licensed using :
+    [License Badge!](https://img.shields.io/static/v1?label=License&message=${data.license}&color=blue)
     
      # Author: ${data.author}
         
      ## Table of Contents
+     *[Decription](#description)
+
      * [Installation](#instalation)
         
      * [Usage](#usage)
-        
+
+     * [License](#license)  
+
      * [Contributing](#contributing)
         
-     * [License](#license)
-        
      * [Tests](#tests)
-        
+     
+     * [Language used](#language used)
+
      * [Questions](#questions)
        
         
         ## Description:
         ${data.description}
+
         ## Installation:
         ${data.instalDep}
-        ## license:
-        [License Badge!](https://img.shields.io/static/v1?label=License&message=${data.license}&color=blue)
-        ## GitHub:
-        ${data.github}
+
+        ## Usage:
+        ${data.usage}
+        
+         ## The following license has been used:
+        ${data.license}
+        
+        ## Contribute:
+        ${data.contribute}
+
+       
+        ## Github:
+        ${data.github} 
+        [GitHub](http://github/${data.github}.com)
+        
         ## language used:
         ${data.language}
+       
         ## Questions:
         Contact ${data.author} with any questions at ${data.contact}
-      `;
-  }
-  
-  module.exports = generateMD;
+      
+        # [![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)]
+
+        `;
+    
+}
+
+module.exports = generateMD;
--- index.js
+++ index.js
@@ -1,95 +1,103 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateMD = require ('./generateMD.js')
+const generateMD = require("./generateMD.js");
 
 // array of questions for user
 const questions = [
-    {
-        type: "input",
-        name: "author",
-        message: "What is your name?"
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your Github username?"
-    },
-    {
-      type: 'input',
-      name: 'contact',
-      message: 'What is your email address?',
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project's title?",
   },
-    {
-        type: "input",
-        name: "title",
-        message: "What is your project's title?"
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Please write a short description of your project."
-    },
-    
-    {
-      type: "input",
-      name: "instalDep",
-      message: "Command to install dependencies?",
-      default: "npm install",
-    },
-    {
-      type: "checkbox",
-      message: "What languages have you used?",
-      name: "language",
-      choices: [
-        {
-          name: "Javascript",
-        },
-        {
-          name: "Python",
-        },
-        {
-          name: "HTML",
-        },
-        {
-          name: "Java",
-        },
-        {
-          name: "C#",
-        },
-        {
-          name: "C",
-        },
-        {
-          name: "C++",
-        },
-        {
-          name: "CSS",
-        },
-      ],
-    },
-    {
-      type: "list",
-      name: "license",
-      message: "What license does your droject require?",
-      choices: ["MIT", "Apache", "GPL", "IBM", "ISC", "Mozilla", "Unlicense"],
-    },
-  ]
+  {
+    type: "input",
+    name: "author",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project.",
+  },
+  {
+    type: "input",
+    name: "instalDep",
+    message: "Command to install dependencies?",
+    default: "npm install",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the intened use of this application?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What license does your droject require?",
+    choices: ["MIT", "Apache", "GPL", "IBM", "ISC", "Mozilla", "Unlicense"],
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message: "Please name anyone who contributed",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your github ?",
+  },
 
+  {
+    type: "checkbox",
+    message: "What languages have you used?",
+    name: "language",
+    choices: [
+      {
+        name: "Javascript",
+      },
+      {
+        name: "Python",
+      },
+      {
+        name: "HTML",
+      },
+      {
+        name: "Java",
+      },
+      {
+        name: "C#",
+      },
+      {
+        name: "C",
+      },
+      {
+        name: "C++",
+      },
+      {
+        name: "CSS",
+      },
+    ],
+  },
 
- // function to initialize program
+  {
+    type: "input",
+    name: "contact",
+    message: "What is your email address?",
+  },
+];
+
+// function to initialize program
 function init() {
-  console.log("Inside the init function")
+  console.log("Inside the init function");
   inquirer.prompt(questions).then((response) => {
-      fs.writeFileSync("exampleREADME.md", generateMD(response) , function (err) {
-
-          if (err) {
-              console.log(err)
-          }
-          else {
-              console.log("Success")
-          }
-      })
-  })
+    fs.writeFileSync("exampleREADME.md", generateMD(response), function (err) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Success");
+      }
+    });
+  });
 }
 
 // function call to initialize program
