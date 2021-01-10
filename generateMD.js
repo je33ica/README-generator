0f21bd3c48655ec5a9a12fb4f5bd0d585e33771a
je33ica <je331687@gmail.com>
2021-01-10 15:44:08 +0000

    spelling mistakes fixed

--- generateMD.js
+++ generateMD.js
@@ -2,56 +2,57 @@
 function generateMD(data) {
   return `# ${data.title}
     # This README has been licensed using :
-    [License Badge!](https://img.shields.io/static/v1?label=License&message=${data.license}&color=blue)
+    [![License Badge](https://img.shields.io/static/v1?label=License&message=${data.license}&color=blue)]
     
-     # Author: ${data.author}
+# Author: ${data.author}
         
-     ## Table of Contents
-     *[Decription](#description)
+ ## Table of Contents
+    *[Description](#description)
 
-     * [Installation](#instalation)
-        
-     * [Usage](#usage)
-
-     * [License](#license)  
-
-     * [Contributing](#contributing)
-        
-     * [Tests](#tests)
-     
-     * [Language used](#language used)
-
-     * [Questions](#questions)
-       
-        
-        ## Description:
-        ${data.description}
-
-        ## Installation:
-        ${data.instalDep}
-
-        ## Usage:
-        ${data.usage}
-        
-         ## The following license has been used:
-        ${data.license}
-        
-        ## Contribute:
-        ${data.contribute}
-
-       
-        ## Github:
-        ${data.github} 
-        [GitHub](http://github/${data.github}.com)
-        
-        ## language used:
-        ${data.language}
-       
-        ## Questions:
-        Contact ${data.author} with any questions at ${data.contact}
+    * [Installation](#instalation)
       
-        # [![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)]
+    * [Usage](#usage)
 
+    * [License](#license)  
+
+    * [Contributing](#contributing)
+      
+    * [Tests](#tests)
+
+    * [Language](#language)
+
+    * [Questions](#questions)
+          
+
+    ## Description:
+    ${data.description}
+
+    ## Installation:
+    ${data.instalDep}
+
+    ## Usage:
+    ${data.usage}
+
+      ## The following license has been used:
+    ${data.license}
+
+    ## Contribute:
+    ${data.contribute}
+
+
+    ## Github:
+    ${data.github} 
+    [GitHub](http://github/${data.github})
+
+    ## language used:
+    ${data.language}
+
+    ## Questions:
+    Contact ${data.author} with any questions at ${data.contact}
+
+    # [![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)]
+
+   
         `;
     
 }
